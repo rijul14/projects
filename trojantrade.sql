92a31c1f382538b4f340f297bfeca9a440612b80
Hye Ri Woo <hyeriwoo@usc.edu>
2021-12-07 12:30:20 -0800

    Add files via upload

--- trojantrade.sql
+++ trojantrade.sql
@@ -10,9 +10,9 @@ CREATE TABLE `trojantrade`.`user` (
   
 CREATE TABLE `trojantrade`.`item` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
   `title` VARCHAR(45) NOT NULL,
-  `image_url` VARCHAR(100) NOT NULL,
-  `time` VARCHAR(100) NOT NULL,
+  `image_url` VARCHAR(500) NOT NULL,
   `price` VARCHAR(45) NOT NULL,
   `location` VARCHAR(100) NOT NULL,
   `description` VARCHAR(400) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `trojantrade`.`item` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
   
-CREATE TABLE `trojantrade`.`postlist` (
+CREATE TABLE `trouserjantrade`.`postlist` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `item_id` INT NOT NULL,
@@ -33,3 +33,16 @@ CREATE TABLE `trojantrade`.`wishlist` (
   `item_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+  
+INSERT INTO user (nickname, username, password, location) VALUES ('Trojan', 'trojan@usc.edu', 'trojan123', 'USC');
+INSERT INTO user (nickname, username, password, location) VALUES ('Hyeri', 'hyeriwoo@usc.edu', 'hyeri123', 'USC');
+
+INSERT INTO item (user_id, title, image_url, price, location, description, category) VALUES (1, 'Selling Macbook Pro', 'https://www.macworld.com/wp-content/uploads/2021/03/macbook-2017-hero-100726395-orig-12.jpg?quality=50&strip=all', '$300', 'USC', 'Almost New', 'electronics');
+INSERT INTO item (user_id, title, image_url, price, location, description, category) VALUES (2, 'Selling CS201 Textbook', 'https://images-na.ssl-images-amazon.com/images/I/51FkY6Xwb8L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg', '$50', 'USC', 'Textbook required for CS201 class!', 'book');
+INSERT INTO item (user_id, title, image_url, price, location, description, category) VALUES (1, 'Mattress', 'https://www.choosemattress.com/wp-content/uploads/2016/06/used-mattress-2-1536x1152.jpg.webp', '$100', 'USC', 'Almost New', 'furniture');
+
+
+
+
+
+
