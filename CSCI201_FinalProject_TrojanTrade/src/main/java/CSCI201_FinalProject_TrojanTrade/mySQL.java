113737e761fe7cb635a151af8f06df477a3898ed
Rijul Raghu <jraghu@usc.edu>
2022-01-17 20:56:52 -0800

    Delete CSCI201_FinalProject_TrojanTrade directory

--- CSCI201_FinalProject_TrojanTrade/src/main/java/CSCI201_FinalProject_TrojanTrade/mySQL.java
+++
@@ -1,7 +0,0 @@
-package CSCI201_FinalProject_TrojanTrade;
-
-public class mySQL {
-	public final static String db = "jdbc:mysql://localhost:3306/trojantrade";
-	public final static String user = "root";
-	public final static String pwd = "j97382q100";
-}
